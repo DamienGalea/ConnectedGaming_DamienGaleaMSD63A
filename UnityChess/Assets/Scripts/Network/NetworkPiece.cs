2873480e1441f37c378d413ae37d7164413b250b
DamienGalea <115038112+DamienGalea@users.noreply.github.com>
2025-04-11 09:57:23 +0200

    Connection

---
+++ UnityChess/Assets/Scripts/Network/NetworkPiece.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class NetworkPiece : NetworkBehaviour
+{
+    public VisualPiece piece;   
+
+    public NetworkVariable<Vector3> syncedPosition = new(
+         writePerm: NetworkVariableWritePermission.Server
+     );
+
+    void Update()
+    {
+       
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void MovePieceServerRpc(Vector3 newPosition)
+    {
+        syncedPosition.Value = newPosition;
+        transform.position = newPosition;
+    }
+
+    
+
+}
+
